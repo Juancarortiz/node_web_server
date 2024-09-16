69246a0c2efd4d1639341e355c95b33706660bab
JC.Ortiz <juancarortizg.mercantil@gmail.com>
2024-09-16 14:12:02 -0400

    - cambios en env.js

--- src/config/env.js
+++ src/config/env.js
@@ -6,6 +6,11 @@ const envs = {
     PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString()
 };
 
+//const envs = {
+//    PORT: get('PORT').required().asPortNumber(),
+//    PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString()
+//};
+
 module.exports = {
     envs
 };
